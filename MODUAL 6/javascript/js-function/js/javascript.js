e25162dc04735cb2072b2a9be750df79b3c9b346
hiren9498 <130139958+hiren9498@users.noreply.github.com>
2023-07-24 11:24:01 +0530

    Add files via upload

---
+++ MODUAL 6/javascript/js-function/js/javascript.js
@@ -0,0 +1,34 @@
+function address1()
+{
+    var add=" Viththal Apartment<br> Race Course Rd, Jagnath Plot<br>Rajkot, Gujarat 360001";
+    document.write(add);
+    document.getElementById("add1").style="color:white; font-size:15px";
+
+}
+
+function address2()
+    {
+        var add="Shop No 6 <br>Kilol Complex<br> Amin Marg, Rajkot, Gujarat 360001";
+        document.write(add);
+        document.getElementById("add2").style="color:white; font-size:15px";
+        
+    };
+    document.write(add);
+    
+ function address3()
+    {
+        var add="B/5, Kld Road<br> Panchnath Camp, Opposite St Mary School<br> Rajkot, Gujarat 360001";
+        document.write(add);
+        document.getElementById("add3").style="color:white; font-size:15px";
+    };
+    document.write(add);
+  
+  function address4()
+    {
+        var add=" Shop No.1, Bombay Super Hight3<br> Kuvadava Rd, near Dmart Mall<br> Rajkot, Sokhada, Gujarat 360003";
+        document.write(add);
+        document.getElementById("add4").style="color:white; font-size:15px";
+        
+    };
+    document.write(add);
+    
